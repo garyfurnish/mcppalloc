4b153be6a502ae2798f2144246565eeffb93ca40
Gary Furnish <gfurnish@gfurnish.net>
2016-12-06 18:48:59 -0700

    initial commit (Doesn't compile)

---
+++ mcppalloc_slab_allocator/mcppalloc_slab_allocator/src/slab_allocator.cpp
@@ -0,0 +1,5 @@
+#include <mcpputil/mcpputil/declarations.hpp>
+// This Must be first.
+#include <mcppalloc/mcppalloc_slab_allocator/slab_allocator.hpp>
+#include <mcppalloc/mcppalloc_slab_allocator/slab_allocator_impl.hpp>
+constexpr const size_t mcppalloc::slab_allocator::details::slab_allocator_t::cs_header_sz;
