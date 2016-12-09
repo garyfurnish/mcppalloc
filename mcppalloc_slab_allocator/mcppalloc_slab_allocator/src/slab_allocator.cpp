5173395b13fa66e63837aeed3131d880f85d9bb6
Gary Furnish <gfurnish@gfurnish.net>
2016-12-09 02:00:18 -0700

    Adjust namespacing/indentation

--- mcppalloc_slab_allocator/mcppalloc_slab_allocator/src/slab_allocator.cpp
+++ mcppalloc_slab_allocator/mcppalloc_slab_allocator/src/slab_allocator.cpp
@@ -2,4 +2,3 @@
 // This Must be first.
 #include <mcppalloc/mcppalloc_slab_allocator/slab_allocator.hpp>
 #include <mcppalloc/mcppalloc_slab_allocator/slab_allocator_impl.hpp>
-constexpr const size_t mcppalloc::slab_allocator::details::slab_allocator_t::cs_header_sz;
