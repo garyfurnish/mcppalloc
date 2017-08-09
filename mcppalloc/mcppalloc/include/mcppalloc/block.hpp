4c92eed4ed64ddf4252b430c4a1acad7d0341255
Gary Furnish <gfurnish@gfurnish.net>
2017-08-09 06:29:27 +0000

    Comments

--- mcppalloc/mcppalloc/include/mcppalloc/block.hpp
+++ mcppalloc/mcppalloc/include/mcppalloc/block.hpp
@@ -6,10 +6,15 @@ namespace mcppalloc
   template <typename Allocator_Policy>
   struct block_t {
     using allocator_policy_type = Allocator_Policy;
-    // using pointer_type = typename ::std::pointer_traits<typename allocator_policy_type::pointer_type>::template rebind<void>;
     using pointer_type = void *;
     using size_type = typename allocator_policy_type::size_type;
+    /**
+     * \brief Pointer returned by raw allocation.
+     **/
     pointer_type m_ptr;
+    /**
+     * \brief Size of allocated memory.
+     **/
     size_type m_size;
     template <typename Return_Allocator_Policy>
     operator block_t<Return_Allocator_Policy>()
