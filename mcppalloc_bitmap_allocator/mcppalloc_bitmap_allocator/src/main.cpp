5c3fb2a86a071afc4f03a2a5cd7adbbb6c1fde69
Gary Furnish <gfurnish@gfurnish.net>
2017-08-29 08:39:35 -0600

    Add missing file

---
+++ mcppalloc_bitmap_allocator/mcppalloc_bitmap_allocator/src/main.cpp
@@ -0,0 +1,11 @@
+#include <mcppalloc/mcppalloc_bitmap_allocator/declarations.hpp>  
+
+#ifdef _WIN32
+namespace mcppalloc::bitmap_allocator
+{
+	__declspec(dllexport) void force_import()
+	{
+
+	}
+}
+#endif
